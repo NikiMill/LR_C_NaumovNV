746e9f9d94fe5298872bcd49aaec01de4771fe0e
NikiMill <108574543+NikiMill@users.noreply.github.com>
2023-05-10 09:39:27 +0300

    Add files via upload

---
+++ LR_№4_NaumovNV/LR_№4_NaumovNV/ИЗ_№11.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LR__4_NaumovNV
+{
+    public partial class ИЗ__11 : Form
+    {
+        public ИЗ__11()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Считывание начальных данных
+            double x0 = Convert.ToDouble(textBox1.Text);
+            double xk = Convert.ToDouble(textBox2.Text);
+            double dx = Convert.ToDouble(textBox3.Text);
+            double a = Convert.ToDouble(textBox4.Text);
+            textBox5.Text = "Работу выполнил ст.Наумов Н.В." +
+            Environment.NewLine;
+            // Цикл для табулирования функции
+            double x = x0;
+            while (x >= (xk + dx / 2))
+            {
+                double y = x + Math.Sqrt(Math.Abs(Math.Pow(x, 3) + a - a * Math.Exp(x)));
+                textBox5.Text += "x=" + Convert.ToString(x) +
+                "; y=" + Convert.ToString(y) + Environment.NewLine;
+                x = x + dx;
+            }
+        }
+
+        private void ИЗ__11_Load(object sender, EventArgs e)
+        {
+            textBox1.Text = "-4";
+            textBox2.Text = "-6,2";
+            textBox3.Text = "-0,2";
+            textBox4.Text = "0,1";
+        }
+    }
+}
---
+++ LR_№4_NaumovNV/LR_№4_NaumovNV/ИЗ_№20.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LR__4_NaumovNV
+{
+    public partial class ИЗ__20 : Form
+    {
+        public ИЗ__20()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Считывание начальных данных
+            double x0 = Convert.ToDouble(textBox1.Text);
+            double xk = Convert.ToDouble(textBox2.Text);
+            double dx = Convert.ToDouble(textBox3.Text);
+            double a = Convert.ToDouble(textBox4.Text);
+            textBox5.Text = "Работу выполнил ст.Наумов Н.В." +
+            Environment.NewLine;
+            // Цикл для табулирования функции
+            double x = x0;
+            while (x <= (xk + dx / 2))
+            {
+                double y = a + Math.Pow(x, 2/3) * Math.Cos(x + Math.Exp(x));
+                textBox5.Text += "x=" + Convert.ToString(x) +
+                "; y=" + Convert.ToString(y) + Environment.NewLine;
+                x = x + dx;
+            }
+        }
+
+        private void ИЗ__20_Load(object sender, EventArgs e)
+        {
+            textBox1.Text = "5,62";
+            textBox2.Text = "15,62";
+            textBox3.Text = "0,5";
+            textBox4.Text = "0,41";
+        }
+    }
+}
