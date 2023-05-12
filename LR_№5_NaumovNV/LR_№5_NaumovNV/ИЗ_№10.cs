6aa4094219c625968d0df99920e0c125b5f4e945
NikiMill <108574543+NikiMill@users.noreply.github.com>
2023-05-12 13:01:02 +0300

    Add files via upload

---
+++ LR_№5_NaumovNV/LR_№5_NaumovNV/ИЗ_№10.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LR__5_NaumovNV
+{
+    public partial class ИЗ__10 : Form
+    {
+        public ИЗ__10()
+        {
+            InitializeComponent();
+        }
+
+        private void ИЗ__10_MouseClick(object sender, MouseEventArgs e)
+        {
+            var i = 0;
+            if (e.Button == MouseButtons.Left)
+            {   //Добавление кнопки
+                Button b = new Button();
+                b.Parent = this;
+                b.Location = new Point(e.X, e.Y);
+                i = i+1;
+            }
+            else if (e.Button == MouseButtons.Right)
+                // Удаление всех кнопок
+                while (i != 0)
+                {
+                    foreach (Control c in this.Controls)
+                        if (c is Button) // Кнопка?
+                            (c as Button).Dispose(); // Да!
+                    i -= 1;
+                }
+    
+        }
+    }
+}
---
+++ LR_№5_NaumovNV/LR_№5_NaumovNV/ТеорЧасть.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LR__5_NaumovNV
+{
+    public partial class ТеорЧасть : Form
+    {
+        public ТеорЧасть()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {   
+            //Создаём кнопочку
+            Button b = new Button();
+            b.Parent = this;
+            //b.Parent = panel1;
+            b.Location = new Point(10, 20);
+            b.Size = new Size(200, 100);
+            // Перебираем все элементы управления
+            foreach (Control c in this.Controls)
+                {
+                if (c is Button) // Кнопка?
+                    (c as Button).Text = "*****"; // Да!
+                }
+        }
+    }
+}
