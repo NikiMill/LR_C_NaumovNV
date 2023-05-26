7c792dca8c8a152643e958699f0a5d2d7d78c2c7
NikiMill <108574543+NikiMill@users.noreply.github.com>
2023-05-26 13:55:54 +0300

    Add files via upload

---
+++ LR_№7_NaumovMV/LR_№7_NaumovMV/ИЗ_№20.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LR__7_NaumovMV
+{
+    public partial class ИЗ__20 : Form
+    {
+        int[] Mas = new int[30];
+        public ИЗ__20()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            // Очищаем элемент управления
+            listBox1.Items.Clear();
+            // Инициализируем класс случайных чисел
+            Random rand = new Random();
+            // Генерируем и выводим 15 элементов
+            for (int i = 0; i < 30; i++)
+            {
+                Mas[i] = rand.Next(-50, 50);
+                listBox1.Items.Add("Mas[" + i.ToString() +
+                "] = " + Mas[i].ToString());
+            }
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            // Очищаем элемент управления
+            listBox2.Items.Clear();
+            // Обрабатываем все элементы
+            for (int i = 0; i < 30; i++)
+            {
+                if (i == 0)
+                {
+                    if (Mas[i] > Mas[i + 1])
+                        listBox2.Items.Add("Mas[" + Convert.ToString(i)
+                         + "] = " + Mas[i].ToString());
+                }
+                else if (i == 29)
+                {
+                    if (Mas[i] > Mas[i - 1])
+                        listBox2.Items.Add("Mas[" + Convert.ToString(i)
+                        + "] = " + Mas[i].ToString());
+                }
+                if ((i != 0) & (i != 29))
+                {
+                    if ((Mas[i - 1] < Mas[i]) & (Mas[i] > Mas[i + 1]))
+                        listBox2.Items.Add("Mas[" + Convert.ToString(i)
+                        + "] = " + Mas[i].ToString());
+                }
+            }                  
+        }
+    }
+}
---
+++ LR_№7_NaumovMV/LR_№7_NaumovMV/ТеорЧасть.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LR__7_NaumovMV
+{
+    public partial class ТеорЧасть : Form
+    {
+        int[] Mas = new int[15];
+        public ТеорЧасть()
+        {
+            InitializeComponent();
+        }
+
+        private void Label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            // Очищаем элемент управления
+            listBox2.Items.Clear();
+            // Обрабатываем все элементы
+            for (int i = 0; i < 15; i++)
+            {
+                if (Mas[i] < 0)
+                    Mas[i] = 0;
+                listBox2.Items.Add("Mas[" + Convert.ToString(i)
+                + "] = " + Mas[i].ToString());
+            }
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            // Очищаем элемент управления
+            listBox1.Items.Clear();
+            // Инициализируем класс случайных чисел
+            Random rand = new Random();
+            // Генерируем и выводим 15 элементов
+            for (int i = 0; i < 15; i++)
+                {
+                    Mas[i] = rand.Next(-50, 50);              
+                    listBox1.Items.Add("Mas[" + i.ToString() +
+                    "] = " + Mas[i].ToString());
+                }
+        }
+
+        private void ListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ListBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
